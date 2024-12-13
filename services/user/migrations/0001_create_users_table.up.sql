dc6d0e43b0412f1266e5dc6563f48d3ed2fce7be
guluzadehh <quluzade.resul13@mail.ru>
2024-12-14 02:02:32 +0400

    Implement migrations

---
+++ services/user/migrations/0001_create_users_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(125),
+    last_name VARCHAR(125),
+    phone VARCHAR(20),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
