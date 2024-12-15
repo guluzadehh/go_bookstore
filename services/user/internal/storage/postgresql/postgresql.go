6bfb4ea87fa0d002410c08ef12efe15749a9fffc
guluzadehh <quluzade.resul13@mail.ru>
2024-12-15 20:46:27 +0400

    Implement CreateUser storage method

--- services/user/internal/storage/postgresql/postgresql.go
+++ services/user/internal/storage/postgresql/postgresql.go
@@ -1,8 +1,13 @@
 package postgresql
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+
+	"github.com/guluzadehh/go_bookstore/services/user/internal/domain/models"
+	"github.com/guluzadehh/go_bookstore/services/user/internal/storage"
+	"github.com/lib/pq"
 )
 
 type Storage struct {
@@ -28,4 +33,38 @@ func New(dsn string) (*Storage, error) {
 
 func (s *Storage) Close() error {
 	return s.db.Close()
+}
+
+func (s *Storage) CreateUser(ctx context.Context, email string, password string) (*models.User, error) {
+	const op = "storage.postgresql.CreateUser"
+
+	var user models.User
+
+	const query = `
+		INSERT INTO users(email, password)
+		VALUES ($1, $2)
+		RETURNING id, email, password, created_at, updated_at, is_active
+	`
+
+	var p string
+
+	err := s.db.QueryRowContext(ctx, query, email, password).Scan(
+		&user.Id,
+		&user.Email,
+		&p,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+		&user.IsActive,
+	)
+	if err != nil {
+		if postgresErr, ok := err.(*pq.Error); ok && postgresErr.Code.Name() == "unique_violation" {
+			return nil, storage.UserExists
+		}
+
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	user.Password = []byte(p)
+
+	return &user, nil
 }
