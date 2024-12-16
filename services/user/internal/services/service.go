5d46ff5616c504d470ccd31fbfd981f7f07cbf5f
guluzadehh <quluzade.resul13@mail.ru>
2024-12-16 23:20:09 +0400

    Implement Authenticate service and grpc handler

--- services/user/internal/services/service.go
+++ services/user/internal/services/service.go
@@ -3,5 +3,6 @@ package service
 import "errors"
 
 var (
-	ErrEmailExists = errors.New("email is already taken")
+	ErrEmailExists        = errors.New("email is already taken")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
